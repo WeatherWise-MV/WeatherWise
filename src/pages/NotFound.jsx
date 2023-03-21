8fef6a16f9f4cc96c468c44912b71025dafcf859
Davies-98 <davies98@protonmail.com>
2023-03-21 16:20:54 +0000

    fixed bugs with 404 error page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import sadFace from "./sad-face.png";
 import "./NotFound.css";
-const NotFound = () => {
 
+const NotFound = () => {
   return (
-    <div className="e404-container">
-      <img src="./asse" className="logo" alt="sad face" />
-      <h1 className="e404-title">404</h1>
-      <p className="e04-subtitle">Page Not Found</p>
-      <p className="e404-message">
+    <div className="not-found-container">
+      <img src={sadFace} alt="Sad face" className="not-found-image" />
+      <h1 className="not-found-title">404</h1>
+      <p className="not-found-subtitle">Page Not Found</p>
+      <p className="not-found-message">
         The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.
       </p>
-      <Link to="/">Return Home</Link>
+      <Link to="/" className="not-found-link">
+        Return Home
+      </Link>
     </div>
   );
 };
