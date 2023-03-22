e840091dce9271dddecf0152b225ea3d4b04e48d
Ethan <ethansteele@hotmail.co.uk>
2023-03-22 16:46:37 +0000

    changes to home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,28 @@
 import React,  { useState, useEffect } from 'react';
-import { Container, HomeLabel, SearchLocationLabel, SearchButton, WeatherBackground } from '../components/homeComponent';
+import { Container, HomeLabel, SearchLocationLabel, SearchButton, SettingsButton, WeatherBackground, ContentContainer, Glow } from '../components/homeComponent';
+import { FaCog } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 function Home() {
+    const [location, setLocation] = useState('');
+
     return (
         <WeatherBackground>
-            <Container>
-                <HomeLabel>WeatherWise</HomeLabel>
-                <SearchLocationLabel>Enter Location for Weather Information</SearchLocationLabel>
-                <SearchButton>
-                    <input placeholder = "Search location..."/>
-                    <button type={"submit"}>Search</button>
-                </SearchButton>
-            </Container>
+            <ContentContainer>
+                <HomeLabel>Welcome to WeatherWise - All Things Weather!</HomeLabel>
+                <Container>
+                    <SearchLocationLabel>Provide location for weather info</SearchLocationLabel>
+                    <SearchButton>
+                        <input placeholder="Search location..." value={location} onChange={(event) => setLocation(event.target.value)}/>
+                        <button type="submit" onClick={() => history.push(`/forecast/${location}`)}>Search</button>
+                    </SearchButton>
+                </Container>
+                <SettingsButton>
+                        <Link to="/settings">
+                            <FaCog size={32} color="white"/>
+                        </Link>
+                </SettingsButton>
+            </ContentContainer>        
         </WeatherBackground>
     )
 }
